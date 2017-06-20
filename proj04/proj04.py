3f2b602622ff0b0971a50e564f7a7f5d2043cdd4
171007314 <kaydenm143@msn.com>
2017-06-20 10:39:15 -0500

    Add files via upload

--- proj04/proj04.py
+++ proj04/proj04.py
@@ -7,4 +7,16 @@ proj04
 Asks the user for a string and prints out whether or not the string is a palindrome.
 
 """
-
+def main():
+    User_String = raw_input("Type a word or phrase: ").lower().replace(" ", "")
+    opp_string = User_String[::-1].lower().replace(" ", "")
+    if opp_string == User_String:
+        print "Your phrase or word is a palindrome"
+    else:
+        print "Your prase or word is not a palindrome"
+    replay = raw_input("Would you like to enter another word? ")
+    if replay == "yes":
+        main()
+    if replay == "no":
+        print "This machine hates you"
+main()
