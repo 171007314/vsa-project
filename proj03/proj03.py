826d66350501f8916d40c994d7432ee32b6718d3
171007314 <kaydenm143@msn.com>
2017-06-20 09:45:36 -0500

    Add files via upload

--- proj03/proj03.py
+++ proj03/proj03.py
@@ -1,6 +1,6 @@
 # Name:
 # Date:
-
+import random
 
 """ 
 proj 03: Guessing Game
@@ -11,5 +11,45 @@ or exactly right. Keep the game going until the user types exit.
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 """
-
+test = random.randint(1,9)
+answer = 10
+wrong_answer = 0
+num_answers = int(raw_input("How many guesses do you want? ")) -1
+while answer != test and wrong_answer <= 4:
+    answer = int(raw_input("Guess a number between one and nine: "))
+    if answer > test:
+        print "You are too high"
+        wrong_answer = wrong_answer + 1
+    if answer < test:
+        print "You are too low"
+        wrong_answer = wrong_answer + 1
+    if answer == test and wrong_answer != 1:
+        print "You got it correct"
+        print "You got",wrong_answer,"questions wrong"
+        restart = raw_input("Would you like to play again? ")
+        if restart == "yes":
+            wrong_answer = 0
+            answer = 10
+            num_answers = int(raw_input("How many guesses do you want? ")) - 1
+        if restart == "no":
+            print "Game Over"
+    if answer == test and wrong_answer == 1:
+        print "You got it correct"
+        print "You got",wrong_answer,"question wrong"
+        restart = raw_input("Would you like to play again? ")
+        if restart == "yes":
+            wrong_answer = 0
+            answer = 10
+            num_answers = int(raw_input("How many guesses do you want? ")) - 1
+        if restart == "no":
+            print "Game Over"
+    if wrong_answer > num_answers:
+        print "You failed this game"
+        restart = raw_input("Would you like to play again? ")
+        if restart == "yes":
+            wrong_answer = 0
+            answer = 10
+            num_answers = int(raw_input("How many guesses do you want? ")) - 1
+        if restart == "no":
+            print "Game Over"
 
