6db9e2c1153fbda215d86cffae98e54094c11c43
171007314 <kaydenm143@msn.com>
2017-06-21 15:40:45 -0500

    Add files via upload

--- proj06/proj06.py
+++ proj06/proj06.py
@@ -45,49 +45,4 @@ def choose_word(wordlist):
 # in the program
 wordlist = load_words()
 
-
 # your code begins here!
-
-def main():
-    x = random.choice(wordlist)
-
-    word = []
-    for letter in x:
-        word.append(letter)
-    print "I am thinking of a " + str(len(word)), "letter word"
-    life = 8
-    a = len(x) * ["_"]
-    while life > 0:
-        player_input = raw_input("Input a single letter: ")
-        player_guess = player_input[0]
-        intersection = [el for el in word if el in player_guess]
-        if intersection == []:
-            print "Sorry, incorrect"
-            life = life - 1
-            print "You lost a life, Lives = " + str(life)
-        elif intersection != []:
-            for i in range(0, len(word)):
-                if word[i] == player_guess:
-                    a[i] = player_guess
-                    print "Good Guess"
-                    print "".join(a)
-
-        if "".join(a) == x:
-            print "Congratulations, you win"
-            playAgain()
-        if life == 0:
-            print "Game Over"
-            print "The word was",x
-            playAgain()
-    main()
-def playAgain():
-    again = raw_input("Would you like to play again? ")
-    if again == 'yes':
-        main()
-    if again == 'no':
-        print "This machine hates you"
-    playAgain()
-main()
-
-
-
