50f75b22a67ed3b9d5b1457dca84277d73559bb4
171007314 <kaydenm143@msn.com>
2017-06-22 15:41:16 -0500

    Add files via upload

--- proj07/scrz.py
+++ proj07/scrz.py
@@ -1,15 +1,13 @@
+import random
 WORDLIST_FILENAME = "words.txt"
+n = 7
+VOWELS = 'aeiou'
+CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
+HAND_SIZE = 7
 
 def load_words():
-    """
-    Returns a list of valid words. Words are strings of lowercase letters.
-    Depending on the size of the word list, this function may
-    take a while to finish.
-    """
     print "Loading word list from file..."
-    # inFile: file
     inFile = open(WORDLIST_FILENAME, 'r', 0)
-    # wordlist: list of strings
     wordlist = []
     for line in inFile:
         wordlist.append(line.strip().lower())
@@ -17,6 +15,43 @@ def load_words():
     return wordlist
 wordlist = load_words()
 
+def display_hand(hand):
+    for letter in hand.keys():
+        for j in range(hand[letter]):
+             print letter
+    print
 
 
-print len(wordlist)
+def deal_hand(n):
+    hand = {}
+    num_vowels = n / 3
+    for i in range(num_vowels):
+        x = VOWELS[random.randrange(0, len(VOWELS))]
+        hand[x] = hand.get(x, 0) + 1
+    for i in range(num_vowels, n):
+        x = CONSONANTS[random.randrange(0, len(CONSONANTS))]
+        hand[x] = hand.get(x, 0) + 1
+    print hand
+    return hand
+hand = deal_hand(n)
+word = raw_input("Enter a word ")
+def is_valid_word(word, hand, wordlist):
+    x = []
+    for letter in word:
+        x.append(letter)
+    y = "".join(x)
+    print y
+    """
+    Returns True if word is in the word_list and is entirely
+    composed of letters in the hand. Otherwise, returns False.
+    Does not mutate hand or word_list.
+
+    word: string
+    hand: dictionary (string -> int)
+    word_list: list of lowercase strings
+    """
+
+
+is_valid_word(word, hand, wordlist)
+
+
